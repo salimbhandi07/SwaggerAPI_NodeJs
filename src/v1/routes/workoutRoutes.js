365a7b89e54208dd25a52ab2f913cee118dba9c1
Salim Bhandi <59732858+salimbhandi07@users.noreply.github.com>
2022-08-25 10:38:18 +0530

    Swagger API using YAML code in Node js

---
+++ src/v1/routes/workoutRoutes.js
@@ -0,0 +1,129 @@
+// const express = require('express');
+// const workoutController = require('../../controllers/workoutController');
+
+// const router = express.Router();
+
+// router.get('/', workoutController.getAllWorkouts);
+
+// router.get('/:workoutId', workoutController.getOneWorkout);
+
+// router.post('/', workoutController.createNewWorkout);
+
+// router.patch('/:workoutId', workoutController.updateOneWorkout);
+
+// router.delete('/:workoutId', workoutController.deleteOneWorkout);
+
+// module.exports = router;
+
+// In src/v1/routes/workoutRoutes.js
+const express = require('express');
+const workoutController = require('../../controllers/workoutController');
+// *** ADD ***
+// const recordController = require('../../controllers/recordController');
+
+const router = express.Router();
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   post:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string
+ *                       example: "Some error message"
+ */
+
+router.get('/', workoutController.getAllWorkouts);
+
+router.get('/:workoutId', workoutController.getOneWorkout);
+
+// *** ADD ***
+// router.get('/:workoutId/records', recordController.getRecordForWorkout);
+
+router.post('/', workoutController.createNewWorkout);
+
+router.patch('/:workoutId', workoutController.updateOneWorkout);
+
+router.delete('/:workoutId', workoutController.deleteOneWorkout);
+
+module.exports = router;
